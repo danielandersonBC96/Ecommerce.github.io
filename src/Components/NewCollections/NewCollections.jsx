d3f3c97784c28e395e284ea742fc4d20933e7a5a
danielandersonBC96 <danielandersonbrandaocampos@hotmail.com>
2024-02-01 16:19:08 -0400

    finalizando o collection

--- src/Components/NewCollections/NewCollections.jsx
+++ src/Components/NewCollections/NewCollections.jsx
@@ -1,8 +1,31 @@
 import React from 'react';
 import './NewCollection.css';
+import new_collection from '../Assets/new_collections'
+import { Item } from '../Items/item';
+
+
 
 export const NewCollections = () => {
   return (
-    <div>NewCollections</div>
+    <div className=' newcollections'>
+        <h1>NEW COLLECTION</h1>
+        <hr/>
+        <div className="collection">
+            {
+                new_collection.map( (item, i ) => {
+
+                     return <Item
+                     key={i } 
+                     id={item.id}
+                     name={item.name}
+                     image={item.image}
+                     new_price={ item.new_price}
+                     old_price={item.old_price}
+                     
+                     />
+                })}
+
+        </div>
+    </div>
   )
 }
