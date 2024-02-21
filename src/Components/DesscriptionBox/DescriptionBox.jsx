00a3de0d4590dc9346dc1ea82b239f23ca8a1c03
danielandersonBC96 <danielandersonbrandaocampos@hotmail.com>
2024-02-21 15:58:49 -0400

    criando layout descrição de produto

---
+++ src/Components/DesscriptionBox/DescriptionBox.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import './DescriptionBox.css';
+
+export const DescriptionBox = () => {
+  return (
+    <div className='descriptionbox'>
+        <div className="description-navigator">
+         <div className="descriptionbox-nav-box"> Product Reviews </div>
+         <div className="descrptionbox-nav-box-fade"> Reviews(123)
+         
+         <div className="descrpitionbox-description">
+        <p>
+            Lorem ipsum dolor sit amet consectetur, 
+            adipisicing elit. Harum vitae laborum voluptates 
+            veniam earum sit sequi ipsam! Laboriosam beatae reprehenderit
+             consequuntur iste aliquid sed nihil,
+             id, nemo quia ab perferendis.
+        </p>
+     </div>
+         
+         
+         </div>
+        </div>
+ 
+    </div>
+  )
+}
