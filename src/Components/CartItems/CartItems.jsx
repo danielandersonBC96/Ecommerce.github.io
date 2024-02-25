6de9ca6ca6914cc2ba43499b0e93f74f2ef06fde
danielandersonBC96 <danielandersonbrandaocampos@hotmail.com>
2024-02-24 21:35:32 -0400

    criando layout do cart items

--- src/Components/CartItems/CartItems.jsx
+++ src/Components/CartItems/CartItems.jsx
@@ -7,10 +7,11 @@ export const CartItems = () => {
 
     const { all_product, cartItem, removeCartItem } = useContext(ShopContext);
 
-
     return (
         <div className='cartitems'>
+            <h1> Cart Items</h1>
             <table className="cartitems-table">
+
                 <thead>
                     <tr>
                         <th>Products</th>
@@ -20,11 +21,13 @@ export const CartItems = () => {
                         <th>Remove</th>
                     </tr>
                 </thead>
+
                 <tbody>
                     {all_product.map((e) => {
                         if (cartItem[e.id] > 0) {
                             return (
                                 <tr className="cartitems-row" key={e.id}>
+
                                     <td>
                                         <img src={e.image} className='carticon-product-icon' alt='product'/>
                                     </td>
@@ -39,6 +42,7 @@ export const CartItems = () => {
                                             className='remove-icon'
                                         />
                                     </td>
+
                                 </tr>
                             );
                         }
@@ -46,11 +50,52 @@ export const CartItems = () => {
                     })}
                 </tbody>
             </table>
+            <div className="cartitems-down">
+                <div className="cartitems-total">
+                    <h1>Cart Totals </h1>
+                    <div>
+                        <div className="cartitems-total-item"> 
+
+                             <p>SubTotal</p>
+                             <p> ${0}</p>
+
+                             <hr/>
+
+                            <div className="caritems-total-item">
+
+                                <p> Shipping Fee </p>
+                                <p>Free</p>
+
+                            </div>         
+                            <hr/> 
+                             <div className="cartitems-total-item">
+
+                                <h3>Total</h3>
+                                <h3>${0}</h3>
+                                
+                             </div>    
+                             <button className='checkout'> Checkout </button>      
+                        </div>
+                       
+                    </div>
+                    <div className="cartitems-promocode">
+      <div className="cartitems-promobox">
+        <div className="left-content">
+          <p>Se você tem um código promocional, insira-o aqui:</p>
+         
+        </div>
+         <div>
+         <input type="text" placeholder="Código promocional" />
+         </div>
+        <div className="right-content">
+      
+          <button className="submit">Enviar</button>
+        </div>
+      </div>
+    </div>
+
+                </div>
+            </div>
         </div>
     );
-    
-    
-        
-        
-
 }
