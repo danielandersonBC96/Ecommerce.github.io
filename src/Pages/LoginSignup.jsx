133adc13297a69d6dba3885dab0c1eb7eb1ac649
danielandersonBC96 <danielandersonbrandaocampos@hotmail.com>
2024-03-29 21:53:47 -0400

    ADDICIONANDO COIDAS NOVAS

--- src/Pages/LoginSignup.jsx
+++ src/Pages/LoginSignup.jsx
@@ -1,14 +1,13 @@
 import React, { useEffect, useState, useContext } from 'react';
 import { useNavigate } from 'react-router-dom';
 import Modal from 'react-modal';
-import '../Css/login.css';
 import { initializeApp } from 'firebase/app';
 import { getAuth, GoogleAuthProvider, signInWithPopup, createUserWithEmailAndPassword } from 'firebase/auth';
 import { RingLoader } from 'react-spinners';
 import { ShopContext } from '../Context/ShopContext';
-
+import { getDatabase, ref, set } from 'firebase/database';
+import '../Css/login.css'
 // Firebase configuration
-
 const firebaseConfig = {
   apiKey: "AIzaSyABRXp9M-W7FiG5KtjQxnaySXhuoJwIPhs",
   authDomain: "ecommerce-cc6b8.firebaseapp.com",
@@ -22,11 +21,21 @@ const spinnerStyles = {
   display: 'block',
   margin: '0 auto'
 };
+const customStyles = {
+  content: {
+    backgroundColor: 'white', // Cor de fundo preto
+    border: 'none', // Sem borda
+    borderRadius: '8px', // Borda arredondada
+    maxWidth: '600px', // Largura máxima
+    margin: 'auto', // Centralizar na tela
+    padding: '20px' // Espaçamento interno
+  }
+};
 
 // Initialize Firebase
-
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
+const db = getDatabase(app);
 
 export const LoginSignup = () => {
   const [modalIsOpen, setModalIsOpen] = useState(false);
@@ -34,9 +43,37 @@ export const LoginSignup = () => {
   const [rememberMe, setRememberMe] = useState(false);
   const [loading, setLoading] = useState(false);
   const [loggedIn, setLoggedIn] = useState(false);
-
   const { addToCart } = useContext(ShopContext);
   const navigate = useNavigate();
+
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    password: '',
+    confirmPassword: ''
+  });
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Escrever os dados no banco de dados Firebase em um caminho específico
+    set(ref(db, "Usuarios"), {
+      name: formData.name,
+      email: formData.email
+    }).then(() => {
+      console.log('Dados cadastrados com sucesso no banco de dados Firebase.');
+    }).catch((error) => {
+      console.error('Erro ao cadastrar os dados no banco de dados Firebase:', error);
+    });
+
+    // Após o envio do formulário, você pode fechar o modal
+    setModalIsOpen(false);
+  };
+
+
 
   useEffect(() => {
     const storedEmail = localStorage.getItem('storedEmail');
@@ -46,7 +83,7 @@ export const LoginSignup = () => {
       setLoginData({ ...loginData, email: storedEmail });
       setRememberMe(true);
     }
-  }, [loginData]); // Adicione loginData como uma dependência
+  }, []); // Remova loginData das dependências para evitar loops infinitos
   
   useEffect(() => {
     const unsubscribe = auth.onAuthStateChanged(user => {
@@ -55,7 +92,6 @@ export const LoginSignup = () => {
       } else {
         setLoggedIn(false);
       }
-      setLoading(false);
     });
 
     return () => unsubscribe();
@@ -76,64 +112,73 @@ export const LoginSignup = () => {
   };
 
   const handleGoogleLogin = async () => {
+    setLoading(true); // Defina loading como true antes de iniciar o login com Google
+
     const provider = new GoogleAuthProvider();
     try {
-      setLoading(true);
       const result = await signInWithPopup(auth, provider);
       const user = result.user;
       console.log('Logged in user:', user);
       createUserAccount(user.email);
+      navigate('/produtos-comprados'); // Redirecione após o login ser concluído
     } catch (error) {
       console.error('Error signing in with Google:', error);
     } finally {
-      setLoading(false);
+      setLoading(false); // Defina loading como false após o login (com sucesso ou falha)
     }
   };
 
   const gerUserByEmail = (email) => {
     const users = [
-      { email: 'progamin@example.com', password: 'userpassword', userType: 'User', id: '1' },
+      { email: 'progamin@example.com', password: 'userpassword', userType: 'user', id: '1' },
       { email: 'admin@example.com', password: 'adminpassword', userType: 'admin', id: '2' }
     ];
     return users.find(user => user.email === email);
   };
 
   const handleLogin = async (itemId) => {
-    setLoading(true);
-    const { email, password } = loginData;
-    const user = gerUserByEmail(email);
+    setLoading(true); // Define loading como true ao clicar no botão de login
 
+    // Simula um login bem-sucedido após 5 segundos
+    setTimeout(async () => {
+      const { email, password } = loginData;
+      const user = gerUserByEmail(email);
 
-    if (user && user.password === password) {
-      if (rememberMe) {
-        localStorage.setItem('storedEmail', email);
-        localStorage.setItem('storedRememberMe', true);
+      if (user && user.password === password) {
+        if (rememberMe) {
+          localStorage.setItem('storedEmail', email);
+          localStorage.setItem('storedRememberMe', true);
+        } else {
+          localStorage.removeItem('storedEmail');
+          localStorage.removeItem('storedRememberMe');
+        }
+
+        if (user.userType === 'admin') {
+          navigate('/cadastrar-produtos');
+        } else {
+          // Aqui você associaria a compra ao usuário logado antes de redirecionar
+      
+          navigate('/produtos-comprados');
+        }
       } else {
-        localStorage.removeItem('storedEmail');
-        localStorage.removeItem('storedRememberMe');
+        alert('Invalid email or password');
       }
 
-      if (user.userType === 'admin') {
-        navigate('/cadastrar-produtos');
-      } else {
-        // Aqui você associaria a compra ao usuário logado antes de redirecionar
-        await addToCart(user.id, itemId);
-        navigate('/produtos-comprados');
-      }
-    } else {
-      alert('Invalid email or password');
-    }
-    setLoading(false);
+      setLoading(false); // Define loading como false após o login ser concluído
+    }, 5000); // 5 segundos de timeout para simular um login demorado
   };
 
   return (
     <div className="loginsignup">
-      {loading ? (
-        <div className="loading-animation">
-          <RingLoader color={'#123abc'} loading={loading} css={spinnerStyles} size={150} />
+       <div style={{ position: 'relative', minHeight: '100vh', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+      {loading && (
+        <div style={{ position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)', textAlign: 'center' }}>
+          <RingLoader color={'#FF0000'} loading={loading} size={150} />
           <p>Loading...</p>
         </div>
-      ) : (
+      )}
+      
+      {!loading && (
         <div className="loginsignup-container">
           <h1>{loggedIn ? 'Welcome back!' : 'Sign Up'}</h1>
           {!loggedIn && (
@@ -161,16 +206,46 @@ export const LoginSignup = () => {
             </div>
           )}
 
-          <button onClick={loggedIn ? null : openModal}>{loggedIn ? 'Continue' : 'Create Account'}</button>
-          <button onClick={loggedIn ? null : () => handleLogin('itemId')}>{loggedIn ? 'Continue' : 'Login'}</button>
+          {!loggedIn && (
+            <>
+              <button onClick={loggedIn ? null : openModal}>{loggedIn ? 'Continue' : 'Create Account'}</button>
+              <button onClick={() => handleLogin('itemId')}>{loggedIn ? 'Continue' : 'Login'}</button>
+            </>
+          )}
         </div>
       )}
-
-      <Modal isOpen={modalIsOpen} onRequestClose={() => setModalIsOpen(false)} contentLabel="Login Modal">
-        <h2>Choose your login option</h2>
-        <button onClick={handleGoogleLogin}>Sign in with Google</button>
-        <button onClick={() => setModalIsOpen(false)}>Close Modal</button>
-      </Modal>
+    </div>
+    <Modal
+        isOpen={modalIsOpen}
+        onRequestClose={() => setModalIsOpen(false)}
+        contentLabel="Cadastro Modal"
+        style={customStyles} // Aplicar os estilos personalizados
+      >
+  <h2>Cadastro</h2>
+  <form onSubmit={handleSubmit}>
+    <div className="form-group">
+      <label htmlFor="name">Nome:</label>
+      <input type="text" id="name" name="name" value={formData.name} onChange={handleChange} required />
+    </div>
+    <div className="form-group">
+      <label htmlFor="email">Email:</label>
+      <input type="email" id="email" name="email" value={formData.email} onChange={handleChange} required />
+    </div>
+    <div className="form-group">
+      <label htmlFor="password">Senha:</label>
+      <input type="password" id="password" name="password" value={formData.password} onChange={handleChange} required />
+    </div>
+    <div className="form-group">
+      <label htmlFor="confirm-password">Confirmar Senha:</label>
+      <input type="password" id="confirm-password" name="confirmPassword" value={formData.confirmPassword} onChange={handleChange} required />
+    </div>
+    <div className="form-group">
+      <button type="submit">Cadastrar</button>
+      <button onClick={() => setModalIsOpen(false)}>x</button>
+    </div>
+  </form>
+ 
+</Modal>
     </div>
   );
 };
